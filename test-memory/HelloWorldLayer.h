783c2ca19fa9c5e2ec93b4008321afbd6860b1bb
Ricardo Quesada <ricardoquesada@gmail.com>
2012-04-05 10:16:13 +0800

    first commit

--- test-memory/HelloWorldLayer.h
+++ test-memory/HelloWorldLayer.h
@@ -7,13 +7,11 @@
 //
 
 
-#import <GameKit/GameKit.h>
-
 // When you import this file, you import all the cocos2d classes
 #import "cocos2d.h"
 
 // HelloWorldLayer
-@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+@interface HelloWorldLayer : CCLayer
 {
 }
 
