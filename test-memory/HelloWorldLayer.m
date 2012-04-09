9aa8256b18cf5c38081e8744c913dde0833952c2
Ricardo Quesada <ricardoquesada@gmail.com>
2012-04-09 11:37:09 -0700

    renamed files

--- test-memory/HelloWorldLayer.m
+++ test-memory/HelloWorldLayer.m
@@ -43,18 +43,27 @@
 
 		CGSize s = [[CCDirector sharedDirector] winSize];
 	
-//		CCSprite *sprite = [CCSprite spriteWithFile:@"texture2048x2048_rgba8888.pvr"];
-		CCSprite *sprite = [CCSprite spriteWithFile:@"texture2048x2048_rgba4444.pvr"];
+		CCSprite *sprite = [CCSprite spriteWithFile:@"texture2048x2048_rgba8888.pvr"];
+//		CCSprite *sprite = [CCSprite spriteWithFile:@"texture2048x2048_rgba4444.pvr"];
 //		CCSprite *sprite = [CCSprite spriteWithFile:@"texture2048x2048_rgb888.pvr"];
 //		CCSprite *sprite = [CCSprite spriteWithFile:@"texture2047x2047_rgba8888.pvr"];
+//		CCSprite *sprite = [CCSprite spriteWithFile:@"texture1536x1536_rgba8888.png"];
 
 		[sprite setPosition:ccp( s.width/2, s.height/2)];
 		[sprite setScale:0.25f];
 		
-		[self addChild:sprite];
+		[self addChild:sprite z:0 tag:1];
+		
+		[self scheduleOnce:@selector(removeSprite:) delay:10];
 
 	}
 	return self;
+}
+
+-(void) removeSprite:(id)sender
+{
+	[self removeChildByTag:1 cleanup:YES];
+	[[CCTextureCache sharedTextureCache] removeUnusedTextures];
 }
 
 // on "dealloc" you need to release all your retained objects
